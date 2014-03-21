01ee3bdb23593cc701f109dab26670045752f65d
root <root@hermes.(none)>
2014-03-20 23:48:03 -0700

    first working version

--- module/zcrypto/crypto.c
+++ module/zcrypto/crypto.c
@@ -19,65 +19,79 @@
 #include <crypto/hash.h>
 #include <sys/zio.h>
 #include <sys/zio_checksum.h>
+#include <crypto/sha.h>
 
-struct shash_desc desc;
+
+
+#define RUN_AND_TEST(fun, error) if (fun < 0) printk(KERN_ERR error);
+
+struct crypto_shash *tfm;
 
 void
 crypto_zio_checksum_SHA256(const void *buf, uint64_t size, zio_cksum_t *zcp)
 {
 	uint64_t hash[4];
+        struct sdesc{
+        	struct shash_desc desc;
+                char ctx[sizeof(struct sha256_state)];
+        } sdesc;
 
-	if (desc.tfm){
-		crypto_shash_init(&desc);
-		crypto_shash_update(&desc, buf, size);
-		crypto_shash_final(&desc, (u8 *) hash);
+        sdesc.desc.flags = 0;
+	sdesc.desc.tfm = tfm;
+	//RUN_AND_TEST(crypto_shash_init(&sdesc.desc), "---- init failed ----");
+	//RUN_AND_TEST(crypto_shash_update(&sdesc.desc, buf, size), "---- crypto finup ----");
+        //RUN_AND_TEST(crypto_shash_final(&sdesc.desc, (u8 *) hash), "---- crypto final ----");
+	crypto_shash_init(&sdesc.desc);
+	crypto_shash_update(&sdesc.desc, buf, size);
+        crypto_shash_final(&sdesc.desc, (u8 *) hash);
+        ZIO_SET_CHECKSUM(zcp,
+                cpu_to_be64(hash[0]),
+                cpu_to_be64(hash[1]),
+                cpu_to_be64(hash[2]),
+                cpu_to_be64(hash[3]));
 
+        /*
+        hash[0] = cpu_to_be64(hash[0]);
+        hash[1] = cpu_to_be64(hash[1]);
+	hash[2] = cpu_to_be64(hash[2]);
+        hash[3] = cpu_to_be64(hash[3]);
 
-		ZIO_SET_CHECKSUM(zcp,
-			cpu_to_be64(hash[0]),
-			cpu_to_be64(hash[1]),
-			cpu_to_be64(hash[2]),
-			cpu_to_be64(hash[3]));
+	if (sdesc.desc.tfm)
+		crypto_free_shash(sdesc.desc.tfm);
+              
+        if ( (zcp)->zc_word[0] != hash[0] || (zcp)->zc_word[1] != hash[1] || 
+        	(zcp)->zc_word[2] != hash[2] || (zcp)->zc_word[3] != hash[3] ){
+	        printk(KERN_ERR "Failed hash with buf %p and size %llu\n", buf, size);
 
-		/*
 		printk(KERN_ERR "Crypto hash %llx:%llx:%llx:%llx\n", zcp->zc_word[0], 
 			zcp->zc_word[1], zcp->zc_word[2], zcp->zc_word[3]);
 
-		zio_checksum_SHA256(buf, size, zcp);
+		printk(KERN_ERR "Zio    hash %llx:%llx:%llx:%llx\n", hash[0], 
+			hash[1], hash[2], hash[3]);
+	}*/
 
-		printk(KERN_ERR "Zio    hash %llx:%llx:%llx:%llx\n", zcp->zc_word[0], 
-			zcp->zc_word[1], zcp->zc_word[2], zcp->zc_word[3]);
-			*/
-
-	} else {
-		zio_checksum_SHA256(buf, size, zcp);
-		return;
-	}
 }
 
 
 
 static int __init init_zcrypto(void){
 
-	struct crypto_shash *tfm;
+	printk(KERN_ERR "zcrypto loaded\n");
 	//TODO move this in an init function
-	tfm =    crypto_alloc_shash("sha256", 0, 0);
+	tfm = crypto_alloc_shash("sha256", 0, 0);
 	if (IS_ERR(tfm)) {
 		printk(KERN_ERR "Failed to load transform for sha256");
-		desc.tfm = NULL;
-		return -1;
+		tfm = NULL;
+		return 1;
 	}
 	printk(KERN_ERR "Loaded module %s\n", crypto_shash_alg(tfm)->base.cra_driver_name );
-	desc.tfm = tfm;
-	desc.flags = CRYPTO_TFM_REQ_MAY_SLEEP; //TODO check this
 	return 0;
 }
 
 
 static void __exit exit_zcrypto(void){
-	if (desc.tfm)
-		crypto_free_shash(desc.tfm);
-
+	if (tfm)
+		crypto_free_shash(tfm);
 }
 
 module_init(init_zcrypto);
